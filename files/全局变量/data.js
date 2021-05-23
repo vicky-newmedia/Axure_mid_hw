ef00c9ceb642756c4c6cf625eac0af11de6e7320
vicky-newmedia <465470638@qq.com>
2021-05-23 20:22:08 +0800

    提交变量作业

---
+++ files/全局变量/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r,s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,N,N,O,P,Q,null,R,S,T,U,V,W,X,S,Y,Z,_(H,I,J,ba),bb,S,bc,bd,_(be,bf,bg,bh,bi,bh,bj,bh,bk,k,J,_(bl,bm,bn,bm,bo,bm,bp,bq)),i,_(j,k,l,k)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="全局变量.html",d="generationDate",e=new Date(1621772510064.23),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="EmailText",s="StateText",t="page",u="packageId",v="f22817300dc4417b81f1d247545f7769",w="type",x="Axure:Page",y="全局变量",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="center",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="near",O="imageRepeat",P="auto",Q="favicon",R="sketchFactor",S="0",T="colorStyle",U="appliedColor",V="fontName",W="Applied Font",X="borderWidth",Y="borderVisibility",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="spread",bl="r",bm=0,bn="g",bo="b",bp="a",bq=0.349019607843137,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
